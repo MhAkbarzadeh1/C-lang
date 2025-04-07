5b05495a20576478d93a9104a44b0ee18317856b
Hesam <ghmahdieakhesam@gmail.com>
2025-04-08 00:28:26 +0330

    Create Exercise 1-9.c

---
+++ Exercise 1-9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+main()
+{
+    int c;
+    int space = 0;
+
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            if (!space) {
+                putchar(c);
+                space = 1;
+            }
+        }
+        else {
+            putchar(c);
+            space = 0;
+        }
+    }
+}
