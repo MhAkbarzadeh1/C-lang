34422087a09b23aec648eac98630be7524df76a4
Hesam <ghmahdieakhesam@gmail.com>
2025-04-10 01:22:12 +0330

    Update f-Exercise 1-13.c

--- f-Exercise 1-13.c
+++ f-Exercise 1-13.c
@@ -1,50 +1,13 @@
 #include <stdio.h>
 
 main() {
-    int c,i;
-    int wordLength = 0;
-    int lengths[20];
-
-    for (i = 0; i < 20; i++) {
-        lengths[i] = 0;
-    }
+    int c;
 
     while ((c = getchar()) != EOF) {
-        if (c == ' ' || c == '\n' || c == '\t') {
-            if (wordLength > 0) {
-                if (wordLength < 20)
-                    lengths[wordLength]++;
-                else
-                    lengths[19]++;
-                wordLength = 0;
-            }
-        }
-        else {
-            wordLength++;
-        }
-    }
-
-    if (wordLength > 0) {
-        if (wordLength < 20)
-            lengths[wordLength]++;
+        if (c == ' ' || c == '\n' || c == '\t')
+            putchar('\n');
         else
-            lengths[19]++;
-    }
-
-    i = 1;
-    while (i < 20) {
-        if (lengths[i] > 0) {
-            printf("len %2d: ", i);
-
-            int stars = lengths[i];
-            while (stars > 0) {
-                printf("*");
-                stars--;
-            }
-
-            printf("\n");
-        }
-        i++;
+            putchar('*');
     }
 
 }
