ae2e929d9ef0cd824fdd18b67627e526745a4535
Hesam <ghmahdieakhesam@gmail.com>
2025-04-18 20:53:12 +0330

    Create f-Exercise 2-2.c

---
+++ f-Exercise 2-2.c
@@ -0,0 +1,9 @@
+for (i = 0; i < lim - 1; ++i) {
+	if ((c = getchar()) != '\n')
+		break;
+		if (c != EOF)
+			break;
+	s[i] = c;
+}
+
+// I don't use ( && , || ) Opreators instead i do it with if statement;
