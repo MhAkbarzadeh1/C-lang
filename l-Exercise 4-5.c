5e75072745190a97c574b6f17311ecbac6b0bf4e
Hesam <ghmahdieakhesam@gmail.com>
2025-05-11 01:34:53 +0330

    Create l-Exercise 4-5.c

---
+++ l-Exercise 4-5.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>  
+
+#define MAXOP   100   
+#define NUMBER  '0'  
+#define FUNCTION 'f'  
+
+int getop(char[]);
+void push(double);
+double pop(void);
+void clearStack(void);
+
+int main(void)
+{
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+        case NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '/':
+            op2 = pop();
+            if (op2 != 0.0)
+                push(pop() / op2);
+            else
+                printf("zero devisor\n");
+            break;
+        case '\n':
+            printf("\t%.8g\n", pop());
+            break;
+
+        case FUNCTION:
+            if (strcmp(s, "sin") == 0)
+                push(sin(pop()));
+            else if (strcmp(s, "exp") == 0)
+                push(exp(pop()));
+            else if (strcmp(s, "pow") == 0) {
+                op2 = pop();
+                push(pow(pop(), op2));
+            }
+            else
+                printf(" unknown function %s\n", s);
+            break;
+
+        default:
+            printf("unknown command %s\n", s);
+            break;
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+
+#define MAXVAL  100 
+
+int sp = 0;         
+double val[MAXVAL];  
+
+void push(double f)
+{
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("stack full, can't push %g\n", f);
+}
+
+double pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("stack empty\n");
+        return 0.0;
+    }
+}
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+int getop(char s[])
+{
+    int i, c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c;     
+    i = 0;
+    if (isdigit(c))  
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.')    
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE]; 
+int  bufp = 0;     
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE)
+        printf("too many characters\n");
+    else
+        buf[bufp++] = c;
+}
+
+void clearStack(void)
+{
+    sp = 0;
+}
